6ec21f1bb222e7e50f51b2821c7359c303b1cf7b
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2025-06-22 16:28:28 +0200

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,27 @@ use rand::Rng;
 use std::{cmp::Ordering, io};
 
 fn main() {
-    let correct = rand::rng().random_range(1..=10);
-    println!("Correct: {correct}");
+    let mut how_many = String::new();
+    println!("How many numbers do you want to guess?");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Error reading input");
+
+    let num_guesses: u8 = how_many.trim().parse().expect("Error ");
+
+    let mut correct = Vec::new();
+
+    for _ in 0..num_guesses {
+        correct.push(rand::rng().random_range(1..=10));
+    }
+
+    println!("{correct:?}");
+
+    let mut guesses_made = 0;
+
     println!("Guess a number");
 
-    loop {
+    while guesses_made < num_guesses {
         let mut guess = String::new();
 
         io::stdin()
@@ -21,13 +37,20 @@ fn main() {
             }
         };
 
-        match guess.cmp(&correct) {
+        match guess.cmp(&correct[guesses_made as usize]) {
             Ordering::Greater => println!("You guessed too high"),
             Ordering::Less => println!("You guessed too low"),
             Ordering::Equal => {
                 println!("You guessed correct!");
-                break;
+                guesses_made += 1;
+                if guesses_made < num_guesses {
+                    println!("Guess the next number");
+                }
             }
         };
+    }
+    println!("Thanks for playing! The correct numbers were:");
+    for item in correct {
+        println!("{item}");
     }
 }
